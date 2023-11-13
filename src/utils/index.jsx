41f8eb129cd8730d1c7628ccd4e835b729082c6d
CookedIdeas <113336281+CookedIdeas@users.noreply.github.com>
2023-11-13 10:02:21 +0100

    Mocking server for test

--- src/utils/index.jsx
+++ src/utils/index.jsx
@@ -3,10 +3,14 @@ import { useMutation, useQuery, useQueryClient } from 'react-query';
 import { toast } from 'react-toastify';
 // import { useMutation, useQuery, useQueryClient } from 'react-query';
 
-const apiUrl = import.meta.env.VITE_API_UTL;
+const apiUrl = import.meta.env.VITE_API_URL;
+const isTest = import.meta.env.VITE_IS_TEST;
+console.log(isTest);
 
+// if meta.env.VITE_IS_TEST === true → baseUrl is 'api/...'
+// if production → baseUrl is meta.env.VITE_API_URL
 export const customFetch = axios.create({
-  baseURL: apiUrl,
+  baseURL: isTest ? 'api' : apiUrl,
 });
 
 export const usePostForm = () => {
